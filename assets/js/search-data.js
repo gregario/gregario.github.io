505a7eeba29d245550f11cf7ca0f22daeb3f4a13
gregario <gregario@users.noreply.github.com>
2026-03-14 19:34:52 +0000

    Deploying to gh-pages from @ gregario/gregario.github.io@db06d08f5633447615063c1385836fd618735645 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Career history, education, and publications.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
